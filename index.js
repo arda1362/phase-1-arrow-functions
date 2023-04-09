ddd2ddfbebbd4e0c0242438e26ab931b98d5a93a
Ardavan Zareirad <ardavan2525@gmail.com>
2023-04-09 02:09:02 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = function(parameter1, parameter2) {
+    return parameter1 / parameter2;
+}
+const square = x => x * x;
+const add = (parameter1, parameter2) => parameter1 + parameter2;
